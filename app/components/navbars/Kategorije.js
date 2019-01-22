e47d65ec3152a94ccfb44b6afa3051d9be90a48a
sadmiraga <sadmirvela@gmail.com>
2019-01-22 09:25:11 +0100

    idk

---
+++ app/components/navbars/Kategorije.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Image, Button } from "react-native";
+
+export default class Kategorije extends Component {
+
+    render() {
+        return (
+            <View>
+                <Text>Glupi vicevi</Text>
+                <Text>Crni humor</Text>
+                <Text>Igra rijeci</Text>
+                <Text>Kod doktora</Text>
+            </View>
+        );
+    }
+}
+
